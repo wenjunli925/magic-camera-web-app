4a508e1bee5b7b660de1173ed4fa24eb77b215e5
Wenjun Li <wenjunli925@gmail.com>
2023-10-05 13:31:46 -0400

    finish the basic interface

--- app.js
+++ app.js
@@ -36,68 +36,78 @@
 //     return { xs, ys };
 // }
 
-async function showMap() {
-    const mymap = L.map('issMap').setView([0, 0], 1);
-    const marker = L.marker([0, 0]).addTo(mymap);
-    const attribution = '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>';
-    const tileUrl = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
-    const tiles = L.tileLayer(tileUrl, { attribution });
-    tiles.addTo(mymap);
+// async function showMap() {
+//     const mymap = L.map('issMap').setView([0, 0], 1);
+//     const marker = L.marker([0, 0]).addTo(mymap);
+//     const attribution = '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>';
+//     const tileUrl = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
+//     const tiles = L.tileLayer(tileUrl, { attribution });
+//     tiles.addTo(mymap);
     
-    getISS(marker);
+//     getISS(marker);
 
-    setInterval(getISS, 1000);
-}
+//     setInterval(getISS, 1000);
+// }
 
 
 
-const api_url = "https://api.wheretheiss.at/v1/satellites/25544";
+// const api_url = "https://api.wheretheiss.at/v1/satellites/25544";
 
-let firstTime = true;
+// let firstTime = true;
 
-async function getISS(marker) {
+// async function getISS(marker) {
     
-    const response = await fetch(api_url);
-    const data = await response.json();
-    const  { latitude, longitude } = data;
+//     const response = await fetch(api_url);
+//     const data = await response.json();
+//     const  { latitude, longitude } = data;
 
-    document.getElementById("lat").textContent = latitude;
-    document.getElementById("lon").textContent = longitude;
-    console.log(latitude, longitude);
+//     document.getElementById("lat").textContent = latitude;
+//     document.getElementById("lon").textContent = longitude;
+//     console.log(latitude, longitude);
 
-    if (firstTime) {
-        marker.setLatLng([latitude, longitude]);
-        firstTime = false;
-    }
-}
+//     if (firstTime) {
+//         marker.setLatLng([latitude, longitude]);
+//         firstTime = false;
+//     }
+// }
 
-async function chartIt() {
-    const data = await getData();
-    const ctx = document.getElementById('chart');
-    new Chart(ctx, {
-      type: 'line',
-      data: {
-        labels: data.xs,
-        datasets: [{
-          label: 'Combined Land-Surface Air and Sea-Surface Water Temperature in °C',
-          data: data.ys,
-          fill: false,
-          borderWidth: 1,
-        }]
-      },
-      options: {
-        scales: {
-          y: {
-            beginAtZero: false,
-            ticks: {
-              // Include a dollar sign in the ticks
-              callback: function(value, index, ticks) {
-                      return value + '°';
-                  }
-            }
-          }
-        }
-      }
-    });
+// async function chartIt() {
+//     const data = await getData();
+//     const ctx = document.getElementById('chart');
+//     new Chart(ctx, {
+//       type: 'line',
+//       data: {
+//         labels: data.xs,
+//         datasets: [{
+//           label: 'Combined Land-Surface Air and Sea-Surface Water Temperature in °C',
+//           data: data.ys,
+//           fill: false,
+//           borderWidth: 1,
+//         }]
+//       },
+//       options: {
+//         scales: {
+//           y: {
+//             beginAtZero: false,
+//             ticks: {
+//               // Include a dollar sign in the ticks
+//               callback: function(value, index, ticks) {
+//                       return value + '°';
+//                   }
+//             }
+//           }
+//         }
+//       }
+//     });
+//   }
+
+  // p5
+  function setup() {
+    noCanvas();
+    const video = createCapture(VIDEO);
+    video.size(1080, 600);
+    video.parent("video-container");
   }
+
+  
 
